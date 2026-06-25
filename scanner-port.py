fdac3d742e636c40430cf65cf6af7fb46b225e81
Root hex <sk9950782@gmail.com>
2026-06-25 23:50:21 +0600

    Add files via upload

---
+++ scanner-port.py
@@ -0,0 +1,101 @@
+import sys, platform
+import socket
+import time
+import threading
+import requests
+from queue import Queue
+gn = '\033[00;32m'
+rd = '\033[00;31m'
+yw = '\033[00;33m'
+Lgn = '\033[01;32m'
+be = '\033[00;34m'
+we = '\033[01;37m'
+
+banner = f"""{yw}
+------------------------------------------
+⣿⣿⣿⣿⠏⠌⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⠀⠀⠸⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⠃⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⡿⠃⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⠃⠀⠀⣾⣿⣿⣿⣿⣿⣦⠀⠈⠻⣿⣿⣿⣿
+⣿⠀⠀⠀⣿⣿⣿⠟⠉⠉⠉⢃⣤⠀⠈⢿⣿⣿
+⣿⠀⠀⠀⢸⣿⡟⠀⠀⠀⠀⢹⣿⣧⠀⠀⠙⣿
+⣿⡆⠀⠀⠈⠻⡅⠀⠀⠀⠀⣸⣿⠿⠇⠀⠀⢸
+⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠔⠛⠁⠀⠀⣠⣿
+⣿⣿⠀⠀root_hex⠀⠀⠀⢀⣴⣿⣿
+⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿
+⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⢴⣠⣿⣿⣿
+⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿
+⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿
+⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿
+{yw}------------------------------------
+"""
+for c in banner:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.005)
+
+print ("")
+print ("")
+print ("")
+
+IP = requests.get('https://get.geojs.io/v1/ip.json')
+myip = IP.json()['ip']
+print(f"{rd}[*]{yw} You ip : {gn}"+myip)
+
+print(f'{rd}[*]{yw} version  :{gn}', platform.version())
+socket.setdefaulttimeout(0.25)
+lock = threading.Lock()
+print (f"{be}____________________________________")
+time.sleep(4)
+print (f"""
+{rd}[-]{yw} Creator channel Telegram: {Lgn}@roothexc
+{rd}[!]{yw}To use, please enter https and http! 
+Example : {gn} Telegram.com
+""")
+print ("")
+time.sleep(2)
+ip_address = input(f"{rd} ┌─["+f"{Lgn}@roothexc"+f"{be}/"+f"{we}port-scanner"+f"""{rd}]
+ └──╼ """+f"{gn}Enter the site address : >>>{yw} ")
+print ("")
+time.sleep(1)
+host = socket.gethostbyname(ip_address)
+print (f'{rd}[+]{yw} IP Address : {gn}', host)
+print (f"{be}____________________________________")
+def scanner(port):
+   tcpsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   try:
+      Connect = tcpsocket.connect((host, port))
+          
+      with lock:
+         time.sleep(2)
+         print(f'{rd}[!]{yw} Port :{Lgn}', port, f'{yw} is open! ')
+
+         print(f"{be}____________________________________")
+      Connect.close()
+   except:
+      pass
+
+def execute():
+   while True:
+      worker = queue.get()
+      scanner(worker)
+      queue.task_done()
+      
+queue = Queue()
+start_time = time.time()
+   
+for x in range(100):
+   thread = threading.Thread(target = execute)
+   thread.daemon = True
+   thread.start()
+   
+for worker in range(1, 500):
+   queue.put(worker)
+   
+queue.join()
+
+print (f"{rd}[*]{gn} telegram account: {Lgn} https://t.me/roothexc")
